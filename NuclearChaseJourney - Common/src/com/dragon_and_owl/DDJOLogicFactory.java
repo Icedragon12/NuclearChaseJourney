6917f3ee633420ab534291597f069dd18ac976ac
Stefan Zaufl <stefan.zaufl@hotmail.com>
2014-05-24 22:00:40 +0200

    Added correct license(MPL 2) Fixed bad project references

--- NuclearChaseJourney - Common/src/com/dragon_and_owl/DDJOLogicFactory.java
+++ NuclearChaseJourney - Common/src/com/dragon_and_owl/DDJOLogicFactory.java
@@ -1,7 +1,8 @@
 /*
- * (C) David Portisch 2014 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 package com.dragon_and_owl;
 
 import com.dragon_and_owl.netowl.common.NetOwlLogicFactory;
